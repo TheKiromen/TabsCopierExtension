a593af7a34ad163a5e996e627f632ed1d3d1a295
Dominik Kruczek <TheKiromen@gmail.com>
2024-08-04 18:33:04 +0200

    Setup manifest and base structure

--- copier.js
+++ copier.js
@@ -1,1 +1,17 @@
-document.body.style.border = "5px solid red";
+// TODO: Update ID's to actual values
+browser.contextMenus.create({
+  id: "1",
+  title: "Copy links from all open tabs",
+  contexts: ["tab"],
+});
+browser.contextMenus.create({
+  id: "2",
+  title: "Copy from selected tab",
+  contexts: ["tab"],
+});
+browser.contextMenus.create({
+  id: "3",
+  title: "Copy to selected tab",
+  contexts: ["tab"],
+  enabled: false,
+});
