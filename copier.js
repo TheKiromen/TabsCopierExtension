c26bf952133c293858fce752c8b6c900844c08e6
Dominik Kruczek <TheKiromen@gmail.com>
2024-08-06 18:25:52 +0200

    Implement tabs copying

--- copier.js
+++ copier.js
@@ -1,51 +1,70 @@
-let index = 0;
+// Copy links from all open tabs in the current window
+async function copyAllTabs(params) {
+  // Get all tabs
+  const tabs = await browser.tabs.query({ currentWindow: true });
 
-async function copyAllLinks(params) {
-  const json = JSON.stringify(params);
-  await navigator.clipboard.writeText(json);
+  // Extract links from tabs
+  let tabsLinks = [];
+  for (const tab of tabs) {
+    if (tab.url) {
+      tabsLinks.push(tab.url);
+    }
+  }
+
+  // TODO: Try to keep list in the same order as tabs in browser
+  // Format links
+  let linksString = "";
+  tabsLinks.forEach((link) => {
+    linksString += link + "\n";
+  })
+
+  // Save to clipboard
+  await navigator.clipboard.writeText(linksString);
 }
 
-async function setStartingTabindex(params) {
-  index++;
-  browser.contextMenus.update(copyFromIndexToSelectedTabId, {enabled: true});
+// Copy start up to selected tab
+async function copyToActiveTab(params) {
+  // Get active tab index
+  const activeTab = await browser.tabs.query({ active: true, currentWindow: true });
 }
 
-async function copyFromIndexToSelectedTab(params) {
-  await navigator.clipboard.writeText("Test value " + index);
+// Copy from selected tab up to the end
+async function copyFromActiveTab(params) {
+  // Get active tab index
+  const activeTab = await browser.tabs.query({ active: true, currentWindow: true });
 }
 
 // Create context menu
-const copyAllLinksId = "copy-all-links";
+const copyAllLinksId = "copy-all-tabs";
 browser.contextMenus.create({
   id: copyAllLinksId,
-  title: "Copy links from all open tabs",
+  title: "Copy all tabs",
   contexts: ["tab"],
 });
-const setStartingTabindexId = "set-starting-tab-index";
+const setStartingTabindexId = "copy-to-active-tab";
 browser.contextMenus.create({
   id: setStartingTabindexId,
-  title: "Copy from selected tab",
+  title: "Copy up to active tab",
   contexts: ["tab"],
 });
-const copyFromIndexToSelectedTabId = "copy-from-index-to-selected-tab";
+const copyFromIndexToSelectedTabId = "copy-from-active-tab";
 browser.contextMenus.create({
   id: copyFromIndexToSelectedTabId,
-  title: "Copy to selected tab",
+  title: "Copy from active tab",
   contexts: ["tab"],
-  enabled: false,
 });
 
 // Setup listeners
 browser.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case copyAllLinksId:
-      copyAllLinks(tab);
+      copyAllTabs(tab);
       break;
     case setStartingTabindexId:
-      setStartingTabindex(tab);
+      copyToActiveTab(tab);
       break;
     case copyFromIndexToSelectedTabId:
-      copyFromIndexToSelectedTab(tab);
+      copyFromActiveTab(tab);
       break;
   }
 })
